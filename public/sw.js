1e92410d8c6cf159aae77902ae6fba040126ed39
Ruan Lauro <ruanlauromonteiro@gmail.com>
2025-03-13 23:23:02 -0300

    update

--- public/sw.js
+++ public/sw.js
@@ -2,8 +2,15 @@ const CACHE_NAME = "pwa-cache-v1";
 const urlsToCache = [
   "/",
   "/index.html",
-  "/style.css",
-  "/app.js"
+  '/src/pages/tools.html',
+  '/src/pages/indexteste.html',
+  "/src/assets/css/style.css",
+  "/app.js",
+  '/src/assets/js/addCategory.js',
+  '/src/assets/js/cota.js',
+  '/src/assets/js/dataMoney.js',
+  '/src/assets/js/main.js',
+  '/src/assets/js/transation.js'
 ];
 
 self.addEventListener("install", (event) => {
